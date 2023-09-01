52af361e379f35b2154a85fe6e342cb0b5721c6a
mohammad hady - khan mohammad pour <41488238+hady-khann@users.noreply.github.com>
2023-09-01 13:17:13 +0330

    Add project files.

---
+++ Easy.StimulReports.Tools.Pdf/Models.cs
@@ -0,0 +1,21 @@
+﻿namespace Easy.StimulReports.Tools.Pdf;
+
+public class ReportInfo<T>
+{
+    public string ReportFileName { get; set; }
+    public string PdfFileName { get; set; }
+    public string FontName { get; set; }
+    public string FontExtension { get; set; } = "ttf";
+}
+
+public class ReportObject<T>
+{
+    public T ObjectData { get; set; }
+    public string BusinessObjectName { get; set; }
+}
+
+public class ReportResult
+{
+    public string PdfFileName { get; set; }
+    public byte[] ReportFile { get; set; }
+}
---
+++ Easy.StimulReports.Tools.Pdf/StmReports.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Stimulsoft.Base;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Export;
+
+namespace Easy.StimulReports.Tools.Pdf;
+
+public class StmReports
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    private readonly IConfiguration _configuration;
+
+    public StmReports(IWebHostEnvironment webHostEnvironment, IConfiguration configuration = null)
+    {
+        _webHostEnvironment = webHostEnvironment;
+        _configuration = configuration;
+        StiLicense.LoadFromString(_configuration["Stimulsoft:License"]);
+    }
+
+    public async Task<ReportResult> PrintPDFAsync<T>(ReportObject<T> Object , ReportInfo<T> info)
+    {
+        StiReport stiReport = new StiReport();
+        string projectRootPath = $"{_webHostEnvironment.WebRootPath}/reports/{info.ReportFileName}.mrt";
+
+        stiReport.Load(projectRootPath);
+        stiReport.RegBusinessObject(Object.BusinessObjectName, Object.ObjectData);
+        await stiReport.RenderAsync();
+
+        MemoryStream memoryStream = new MemoryStream();
+        StiFontCollection.AddFontFile($"{_webHostEnvironment.WebRootPath}/fonts/{info.FontName}.{info.FontExtension}");
+        stiReport.LoadFonts();
+
+        StiOptions.Export.Pdf.AllowImportSystemLibraries = true;
+        stiReport.ExportDocument(StiExportFormat.Pdf, memoryStream, new StiPdfExportSettings()
+        {
+            Compressed = true,
+            AllowEditable = StiPdfAllowEditable.No,
+            ImageQuality = 1,
+            EmbeddedFonts = true,
+            StandardPdfFonts = true,
+            ImageCompressionMethod = StiPdfImageCompressionMethod.Jpeg
+        });
+
+        byte[] bytesInStream = memoryStream.ToArray();
+        await memoryStream.WriteAsync(bytesInStream, 0, bytesInStream.Length);
+        memoryStream.Position = 0;
+
+        return new ReportResult
+        {
+            ReportFile = memoryStream.GetBuffer(),
+            PdfFileName = info.PdfFileName
+        };
+    }
+
+    public async Task<ReportResult> PrintPDFAsync<T>(List<ReportObject<T>> Object, ReportInfo<T> info)
+    {
+        StiReport stiReport = new StiReport();
+        string projectRootPath = $"{_webHostEnvironment.WebRootPath}/reports/{info.ReportFileName}.mrt";
+
+        stiReport.Load(projectRootPath);
+        Object.ForEach(x =>
+        {
+            stiReport.RegBusinessObject(x.BusinessObjectName, x.ObjectData);
+        });
+
+        await stiReport.RenderAsync();
+
+        MemoryStream memoryStream = new MemoryStream();
+
+        StiFontCollection.AddFontFile($"{_webHostEnvironment.WebRootPath}/fonts/{info.FontName}.{info.FontExtension}");
+
+        stiReport.LoadFonts();
+
+        StiOptions.Export.Pdf.AllowImportSystemLibraries = true;
+        stiReport.ExportDocument(StiExportFormat.Pdf, memoryStream, new StiPdfExportSettings()
+        {
+            Compressed = true,
+            AllowEditable = StiPdfAllowEditable.No,
+            ImageQuality = 1,
+            EmbeddedFonts = true,
+            StandardPdfFonts = true,
+            ImageCompressionMethod = StiPdfImageCompressionMethod.Jpeg
+        });
+
+        byte[] bytesInStream = memoryStream.ToArray();
+        await memoryStream.WriteAsync(bytesInStream, 0, bytesInStream.Length);
+        memoryStream.Position = 0;
+
+        return new ReportResult
+        {
+            ReportFile = memoryStream.GetBuffer(),
+            PdfFileName = info.PdfFileName
+        };
+    }
+}
